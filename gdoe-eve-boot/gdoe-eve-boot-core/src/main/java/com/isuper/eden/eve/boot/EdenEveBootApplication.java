d5481accf49a43977921d143e2020e2cbc3d5509
yongle.zou <yongle.zou@fsg.com.cn>
2021-08-24 15:53:40 +0800

    支持fsgplus

--- gdoe-eve-boot/gdoe-eve-boot-core/src/main/java/com/isuper/eden/eve/boot/EdenEveBootApplication.java
+++ gdoe-eve-boot/gdoe-eve-boot-core/src/main/java/com/isuper/eden/eve/boot/EdenEveBootApplication.java
@@ -9,25 +9,27 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-/** @author admin */
+/**
+ * @author admin
+ */
 @EnableAsync
 @EnableRetry
-@ComponentScan(basePackages = {"com.isuper"})
+@ComponentScan(basePackages = {"com.isuper", "com.fsg", "com.fsgplus"})
 @SpringBootApplication
 @EnableConfigurationProperties
 public class EdenEveBootApplication {
 
-  public static String[] args;
-  public static ConfigurableApplicationContext context;
+    public static String[] args;
+    public static ConfigurableApplicationContext context;
 
-  public static void start(String[] args) {
-    EdenEveBootApplication.args = args;
-    EdenAdamBootstrap.run(args);
-    EdenEveBootApplication.context = SpringApplication.run(EdenEveBootApplication.class, args);
-    EdenAdamBootstrap.markStartupAndWait();
-  }
+    public static void start(String[] args) {
+        EdenEveBootApplication.args = args;
+        EdenAdamBootstrap.run(args);
+        EdenEveBootApplication.context = SpringApplication.run(EdenEveBootApplication.class, args);
+        EdenAdamBootstrap.markStartupAndWait();
+    }
 
-  public static void main(String[] args) {
-    EdenEveBootApplication.start(args);
-  }
+    public static void main(String[] args) {
+        EdenEveBootApplication.start(args);
+    }
 }
